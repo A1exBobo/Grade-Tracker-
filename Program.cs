99ac14c7aecf2f8291f2107c444a5c62e53a020c
A1exBobo <bobocalexandru55@gmail.com>
2026-07-05 13:46:39 +0300

    Extra verificari

--- Program.cs
+++ Program.cs
@@ -38,3 +38,43 @@ float mediePclp = PCLP.CalculMedieMaterie();
 Console.WriteLine($"Media la materia {PCLP.Nume} este: {mediePclp}.");
 Console.WriteLine($"Media la laboratorul {PCLP.Nume} este: {medieLaborator}.");
 Console.WriteLine($"Media la cursul {PCLP.Nume} este: {medieCurs}.");
+
+
+//------------------------------------------------------------------------
+Laborator LabSSC = new Laborator
+{
+    NoteLaborator = new float[] { 7.0f, 7.0f },
+    notaProiect = 7.83f,
+    PondereProiect = 60,
+    notaSeminar = 8.0f,
+    PondereSeminar = 20
+};
+
+Curs CursSSC = new Curs
+{
+    areDistribuita = true,
+    NoteDistribuite = new Tuple<float, float>[]
+    {
+        new Tuple<float, float>(5.0f, 5.0f),          //note curente
+        new Tuple<float, float>(7.0f, 7.0f)          //posibile note 
+    }
+};
+
+Materie SSC = new Materie(
+    nume: "Securitatea sistemelor de calcul",
+    numarCredite: 5,
+    pondereCurs: 0.6f,
+    laborator: LabSSC,
+    labHasProject: false,
+    labHasSeminar: false,
+    cursHasDistribuita: true,
+    curs: CursSSC
+);
+
+float medieLaboratorSSC = LabSSC.CalculMedieLaborator(SSC.LabHasProject, SSC.LabHasSeminar);
+float medieCursSSC = CursSSC.CalculMedieCurs(CursSSC.areDistribuita);
+float medieSSC = SSC.CalculMedieMaterie();
+
+Console.WriteLine($"Media la materia {SSC.Nume} este: {medieSSC}.");
+Console.WriteLine($"Media la laboratorul {SSC.Nume} este: {medieLaboratorSSC}.");
+Console.WriteLine($"Media la cursul {SSC.Nume} este: {medieCursSSC}.");
